7c90f2cd90a9f79528616c58e814eba812de7336
Matarawy <mahmoudsarhan02672@gmail.com>
2023-04-24 02:24:16 +0200

    update

--- Air Conditioner/Air Conditioner/HAL/timer_manager/timer_manager.c
+++ Air Conditioner/Air Conditioner/HAL/timer_manager/timer_manager.c
@@ -13,17 +13,54 @@
 
 u8_en_timerErrorsType TIMER_Manager_init (st_timerConfigType* st_config)
 {
-	return 0;
+	u8_en_timerErrorsType l_ret = TIMER_E_OK;
+	if(NULL==st_config || st_config->u8_timerNum >= TIMER_INVALID_NUM || st_config->u8_timerClock >= TIMER_INVALID_CLOCK)
+	{
+		l_ret = TIMER_E_NOT_OK;
+	}
+	else
+	{
+		l_ret  = TIMER_init(st_config);
+		l_ret |= TIMER_setCallBack(st_config->callBackFunction,st_config->u8_timerNum);
+	}
+	return l_ret;
 }
 u8_en_timerErrorsType TIMER_Manager_start (st_timerConfigType* st_config)
 {
-	return 0;
+	u8_en_timerErrorsType l_ret = TIMER_E_OK;
+	if(NULL==st_config || st_config->u8_timerNum >= TIMER_INVALID_NUM || st_config->u8_timerClock >= TIMER_INVALID_CLOCK)
+	{
+		l_ret = TIMER_E_NOT_OK;
+	}
+	else
+	{
+		l_ret = TIMER_start(st_config);
+	}
+	return l_ret;
 }
 u8_en_timerErrorsType TIMER_Manager_stop (u8_en_timerNumberType u8_en_timerNum)
 {
-	return 0;
+	u8_en_timerErrorsType l_ret = TIMER_E_OK;
+	if( u8_en_timerNum >= TIMER_INVALID_NUM )
+	{
+		l_ret = TIMER_E_NOT_OK;
+	}
+	else
+	{
+		l_ret = TIMER_stop(u8_en_timerNum);
+	}
+	return l_ret;
 }
 u8_en_timerErrorsType TIMER_Manager_reset (st_timerConfigType* st_config)
 {
-	return 0;
+	u8_en_timerErrorsType l_ret = TIMER_E_OK;
+	if(NULL==st_config || st_config->u8_timerNum >= TIMER_INVALID_NUM || st_config->u8_timerClock >= TIMER_INVALID_CLOCK)
+	{
+		l_ret = TIMER_E_NOT_OK;
+	}
+	else
+	{
+		l_ret = TIMER_reset(st_config);
+	}
+	return l_ret;
 }
